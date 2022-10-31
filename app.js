3ae7f9a63275133f4957dfdab6ff776d5c56b0a2
Artem Mokhov <teumik@gmail.com>
2022-10-31 16:00:02 +0400

    refactor: added concise and meaningful names of classes and tags according to rule 3.2 and 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -85,11 +85,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
+    var containsClass=listItem.classList.contains("edited");
+    //If class of the parent is .edited
     if(containsClass){
 
-        //switch to .editmode
+        //switch to .edited
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +98,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    //toggle .edited on the parent.
+    listItem.classList.toggle("edited");
 };
 
 
